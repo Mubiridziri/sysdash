a83659e3ec71f17b29f1f3d564d944644836913a
Yury Potapov <yury.potapov@satellite-soft.ru>
2023-02-02 02:22:07 +0400

    Temporary comment code in entrypoint

--- docker/entrypoints/entrypoint-app.sh
+++ docker/entrypoints/entrypoint-app.sh
@@ -3,11 +3,11 @@
 (cd back && composer install && bin/console cache:clear --env=prod)
 (npm install && npm run build)
 
-if back/bin/console doctrine:migration:status | grep -E -i '^\|\s+\|\s+Current\s+\|\s+0'; then
-  sh back/deploy.sh
-  else
-    back/bin/console doctrine:migrations:migrate -n
-fi
+#if back/bin/console doctrine:migration:status | grep -E -i '^\|\s+\|\s+Current\s+\|\s+0'; then
+#  sh back/deploy.sh
+#  else
+#    back/bin/console doctrine:migrations:migrate -n
+#fi
 
 echo "----------------------------------------"
 echo -e 'Starting complete!'
