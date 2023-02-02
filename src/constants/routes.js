5ae6b1f94c2aff00546f8f16871255ecae3d2527
Yury Potapov <yury.potapov@satellite-soft.ru>
2023-02-02 21:57:21 +0400

    Added menu

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,8 +1,15 @@
 export const EXTERNAL_SYSTEMS = "external_systems";
+export const CLASSIFIERS = "classifiers";
 
 export const ROUTES = [
   {
     id: EXTERNAL_SYSTEMS,
+    title: 'Мониторинг',
     path: `/${EXTERNAL_SYSTEMS}`,
+  },
+  {
+    id: CLASSIFIERS,
+    title: 'Классификаторы',
+    path: `/${CLASSIFIERS}`,
   },
 ];
